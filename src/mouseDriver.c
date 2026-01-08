8c6500df912e922d30673ad886fecd14d1494eb7
Kravataf <159631603+Kravataf@users.noreply.github.com>
2026-01-08 22:03:50 +0100

    Update mouseDriver.c

--- src/mouseDriver.c
+++ src/mouseDriver.c
@@ -1,3 +1,15 @@
 #include "../include/mouseDriver.h"
 int mouseX = 0;
 int mouseY = 0;
+
+// https://wiki.osdev.org/Mouse_Input
+
+// data in port 0x60
+// 0x64 bit n0 == 1 = data available
+// n1 == 0x20 = from mouse
+
+// formatting of bytes
+// 1 = Y overflow, X overflow, Y sign bit, X sign bit, Always 1, Middle Btn, Right Btn, Left Btn
+// 2 = X movement
+// 3 = Y movement
+// 4 = optional
